adc02c00066015f143598684a36dfc8338017c91
ajoossharma <165235263+ajoossharma@users.noreply.github.com>
2024-08-27 01:29:27 +0530

    added more components on the shop page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import { Shop } from './pages/Shop';
 import { ShopCategory } from './pages/ShopCategory';
 import { Product } from './pages/Product';
 import { LoginSignup } from './pages/LoginSignup';
+import { Cart } from './pages/Cart';
+
 
 function App() {
   return (
@@ -18,13 +20,13 @@ function App() {
         <Route path='/women' element={<ShopCategory category="women" />}/>
         <Route path='/kids' element={<ShopCategory category="kid"/>}/>
         <Route path = "/product" element={<Product/>}>
-        <Route path = ' :productId' element={<Product/>}>
+        <Route path = ':productId' element={<Product/>}/>
       
         </Route>
 
-        <Route path='/cart' element={<Cart/>}/>
+        <Route path = '/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSignup/>}/>
-        
+          
       </Routes>
       </BrowserRouter>
       
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -12,14 +12,14 @@ export const Navbar = () => {
             <p>SHOPPER</p>
         </div>
         <ul className="nav-menu">
-            <li onClick={()=>{setMenu("shop")}}><Link to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("men")}}><Link to='/Men'>Men</Link>{menu==="men"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("women")}}><Link to="Women">Women</Link>{menu==="women"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("kids")}}><Link to="kids">kids</Link>{menu==="kids"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration: 'none' }} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("men")}}><Link style={{textDecoration: 'none' }} to='/Men'>Men</Link>{menu==="men"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("women")}}><Link style={{textDecoration: 'none' }} to="Women">Women</Link>{menu==="women"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}><Link style={{textDecoration: 'none' }} to="kids">kids</Link>{menu==="kids"?<hr/>:<></>}</li>
         </ul>
         <div className="nav-login-cart">
             <Link to='/login'><button>Login</button></Link>
-            <img src={cart_icon} alt="" />
+            <link to ='/cart'><img src={cart_icon} alt="" /></link>
             <div className="nav-cart-count">0</div>
         </div>
 
