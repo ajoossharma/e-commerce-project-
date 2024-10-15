26fe28c3762e70f233ebd1d3bd45ee758074801c
ajoossharma <165235263+ajoossharma@users.noreply.github.com>
2024-10-15 20:04:05 +0530

    completed Admin Panel
    also coded more API endpoints eg:-login
    made user Schema etc;

--- backend/index.js
+++ backend/index.js
@@ -127,6 +127,75 @@ app.get('/allproducts' ,async (req,res)=>{
 
 })
 
+//Schema creating for user model 
+
+const Users = mongoose.model('Users', {
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type:String,
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+
+})
+
+//Creating Endpoint for registering the user
+app.post('/signup',async (req,res)=>{
+
+    let check = await Users.findOne({email:req.body.email});
+    if (check) {
+        return res.status(400).json({success:false,errors:"existing user found with same email address"})
+    }
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+        cart[i] = 0;
+    }
+    const user = new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+
+    await user.save();
+
+    const data = {
+        user:{
+            id:user.id
+        }
+    }
+
+    const token = jwt.sign(data,'secret_ecom');
+    res.json({success:true,token})
+
+})
+
+//creatin endpoint for user login
+app.post('/login', async (req,res)=>{
+    let user = await Users.findOne({email:req.body.email});
+    if(user) {
+        const passCompare = req.body.password === user.password;
+        if (passCompare) {
+            const data = {
+                user:{
+                    id:user.id
+                }
+            }
+        }
+    }
+})
+
 app.listen(port,(error)=>{
     if (!error) {
         console.log("Server running on Port" +port)
