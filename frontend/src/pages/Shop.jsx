4dc97ee7aecaf70af12a39ce079499dff0ab40df
ajoossharma <165235263+ajoossharma@users.noreply.github.com>
2024-11-01 21:42:24 +0530

    added more endpoints
    and connected backend APIs with frontend

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Hero } from '../components/Hero/Hero'
-import { Popular } from '../components/Popular/Popular'
 import { Offers } from '../components/Offers/Offers'
 import NewCollections from '../components/NewCollections/NewCollections'
 import NewsLetter from '../components/NewsLetter/NewsLetter'
+import Popular from '../components/Popular/Popular'
 
-export const Shop = () => {
+const Shop = () => {
   return (
     <div>
       <Hero/>
-      <Popular/>
+      <Popular/>  
       <Offers/>
       <NewCollections/>
       <NewsLetter/>
